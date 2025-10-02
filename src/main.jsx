0527e761b4d15257e50740ef550a6a123d736e7e
Maher-Elmair <maher.elmair.dev@gmail.com>
2025-10-02 17:51:14 +0300

    🔧 Fix Vite base path

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,6 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const client_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
-// const client_ID = "554478603837-aotj43h50ie8b7eukeq3u506430n63e3.apps.googleusercontent.com";
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <GoogleOAuthProvider clientId={client_ID}>
